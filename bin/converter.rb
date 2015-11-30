3741a645efed9548c67a90cb6487b4292e43fa35
koshikawa <koshikawa.naoto@moneyforward.co.jp>
2015-11-30 16:56:17 +0900

    escape /

--- bin/converter.rb
+++ bin/converter.rb
@@ -1,5 +1,5 @@
 #! /usr/bin/env ruby
 
-word = Regexp.escape($_.chomp)
+word = Regexp.escape($_.chomp).gsub('/', '\/')
 file = `git grep -l '#{word}'`
 `sed -i '' 's/#{word}/!!!CREDENTIAL!!!/g' #{file}` if file !=""

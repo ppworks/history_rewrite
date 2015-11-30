e2c8b828f526c84b1f80cd61369ed98bb2da9a23
koshikawa <koshikawa.naoto@moneyforward.co.jp>
2015-11-30 13:55:11 +0900

    Escape text for regexp

--- bin/converter.rb
+++ bin/converter.rb
@@ -1,5 +1,5 @@
 #! /usr/bin/env ruby
 
-word = $_.chomp
-file = `git grep -l #{word}`
-`sed -i "" "s/#{word}/!!!CREDENTIAL!!!/g" #{file}` if file !=""
+word = Regexp.escape($_.chomp)
+file = `git grep -l '#{word}'`
+`sed -i '' 's/#{word}/!!!CREDENTIAL!!!/g' #{file}` if file !=""

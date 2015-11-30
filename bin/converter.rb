fc09fd514356d879c81ba74e7f1bf1a8d120ad45
koshikawa <koshikawa.naoto@moneyforward.co.jp>
2015-11-30 16:56:38 +0900

    grep result has multiple lines

--- bin/converter.rb
+++ bin/converter.rb
@@ -1,5 +1,7 @@
 #! /usr/bin/env ruby
 
 word = Regexp.escape($_.chomp).gsub('/', '\/')
-file = `git grep -l '#{word}'`
-`sed -i '' 's/#{word}/!!!CREDENTIAL!!!/g' #{file}` if file !=""
+files = `git grep -l '#{word}'`.split("\n")
+files.each do |file|
+  `sed -i '' 's/#{word}/!!!CREDENTIAL!!!/g' #{file}` if file !=""
+end
